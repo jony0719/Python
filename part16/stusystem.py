3f109e1532c7f68194eab99066a6b8fdfcb54b59
jony0719 <w19948989@163.com>
2022-02-11 01:51:16 +0800

    修改学生信息

--- part16/stusystem.py
+++ part16/stusystem.py
@@ -45,7 +45,7 @@ def menu():
     print('            5.排序                      ')
     print('            6.统计学生总人数              ')
     print('            7.显示所有学生信息            ')
-    print('            8.退出                      ')
+    print('            0.退出                      ')
     print('=======================================')
 
 
@@ -130,7 +130,35 @@ def delete():
 
 
 def modify():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(filename, 'r', encoding='utf-8') as rfile:
+            student_old = rfile.readlines()
+    else:
+        return
+    student_id = input('请输入要修改的学员的ID:')
+    with open(filename, 'w', encoding='utf-8') as wfile:
+        for item in student_old:
+            d = dict(eval(item))
+            if d['id'] == student_id:
+                print('找到学生信息，可以修改学生信息了')
+                while True:
+                    try:
+                        d['name'] = input('请输入姓名:')
+                        d['english'] = input('请输入英语成绩:')
+                        d['python'] = input('请输入python成绩')
+                        d['java'] = input('请输入java成绩')
+                    except:
+                        print('您输入的有误，请重新输入!!!')
+                    else:
+                        break
+                wfile.write(str(d) + '\n')
+                print('修改成功！！！')
+            else:
+                wfile.write(str(d) + '\n')
+        answer = input('是否需要修改其他学生信息?y/n')
+        if answer == 'y':
+            modify()
 
 
 def sort():
