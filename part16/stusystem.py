a5eb2555552a63335d4b8d344d1aa6b05ba5a589
jony0719 <w19948989@163.com>
2022-02-13 23:52:19 +0800

    重新定义标题显示格式

--- part16/stusystem.py
+++ part16/stusystem.py
@@ -136,7 +136,7 @@ def show_student(lst):
         return
     # 定义标题显示格式
     format_title = '{:^6}\t{:^12}\t{:^8}\t{:^10}\t{:^10}\t{:^8}'
-    print(format_title.format('ID', '姓名', '英语成绩', 'Python成绩', 'Java成绩', '总成绩'))
+    print(format_title.format('学生ID', '姓名', '英语成绩', 'Python成绩', 'Java成绩', '总成绩'))
     # 定义内容的显示格式
     format_data = '{:^6}\t{:^12}\t{:^8}\t{:^8}\t{:^8}\t{:^8}'
     for item in lst:
